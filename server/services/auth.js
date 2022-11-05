4f6a10dd4c2a8eaee49c1f8ae60588a187838345
sunday00 <kind65505256@gmail.com>
2022-11-05 21:09:41 +0900

    :construction: koa server time
    koa server time

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,6 +1,8 @@
 import {showWhere} from "../external/index.js";
 import btoa from 'btoa'
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 export default {
   async login(ctx){
@@ -10,10 +12,13 @@ export default {
       secret_code: { type: 'string', content: ctx.request.body.code }
     }).then(res => {
       if(res.data?.results[0]?.properties) {
+        dayjs.extend(utc)
+        dayjs.extend(timezone)
+
         ctx.status = 200
         ctx.body = {
           id: res?.data?.results[0].id,
-          token: btoa(`${ctx.request.body.nickname}.${process.env.VITE_SALT}.${res?.data?.results[0].id}.${dayjs().add(30, 'minutes').format('YYYY-MM-DDTHH:mm:ss')}`),
+          token: btoa(`${ctx.request.body.nickname}.${process.env.VITE_SALT}.${res?.data?.results[0].id}.${dayjs().tz("Asia/Seoul").add(30, 'minutes').format('YYYY-MM-DDTHH:mm:ss')}`),
           name:  res.data?.results[0]?.properties.realname.rich_text[0].plain_text,
         }
       } else {
